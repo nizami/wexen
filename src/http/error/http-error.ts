01bb992c5fc2a60acf8b46e981878ef1656d0d59
nizami <nizami.dev@gmail.com>
2025-07-13 19:35:45 +0300

    Update http error

--- src/http/error/http-error.ts
+++ src/http/error/http-error.ts
@@ -1,12 +1,7 @@
-import {HttpMethod, HttpStatusCode} from '#wexen';
+import {HttpStatusCode} from '#wexen';
 
 export class HttpError extends Error {
-  constructor(
-    public statusCode: HttpStatusCode,
-    public method: HttpMethod,
-    public url: string,
-    message: string,
-  ) {
-    super(`${statusCode} ${method} ${url} ${message}`);
+  constructor(public statusCode: HttpStatusCode, message: string) {
+    super(message);
   }
 }
